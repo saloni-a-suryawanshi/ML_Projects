cd80be837a140215cee4b7aa0ba556e70a85aadc
Saloni Suryawanshi <saloniasuryawanshi22@gmail.com>
2025-11-19 11:20:30 +0530

    Add files via upload

---
+++ yolo/2.yolov8_basics.py
@@ -0,0 +1,14 @@
+from ultralytics import YOLO
+import numpy
+
+# load a pretrained YOLOv8n model
+model = YOLO("yolov8n.pt", "v8")  
+
+# predict on an image
+detection_output = model.predict(source=r"C:\Users\Saloni\Desktop\photos\boyy.jpeg", conf=0.25, save=True) 
+
+# Display tensor array
+print(detection_output)
+
+# Display numpy array
+# print(detection_output[0].numpy())
